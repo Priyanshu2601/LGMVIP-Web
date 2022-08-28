1fbadbca1d1d33d2124c44c189eca3543f3ee18e
Priyanshu <priyanshusharma2601@gmail.com>
2022-08-28 17:40:56 +0530

    todo

---
+++ task1/src/App.js
@@ -0,0 +1,12 @@
+import TodoList from './Components/TodoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Todo-list-container">
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
